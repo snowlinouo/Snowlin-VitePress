940fbb98e10014ae47888f7f616f82740793288d
雪鈴 SnowLin <113241163+snowlinouo@users.noreply.github.com>
2024-02-17 11:16:51 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/Snowlin-VitePress/",
   title: "SnowLin",
   description: "測試網站",
   themeConfig: {
