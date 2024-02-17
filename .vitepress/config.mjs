7379c827d688c3123eda68b92177e797a148f7b5
雪鈴 SnowLin <113241163+snowlinouo@users.noreply.github.com>
2024-02-17 09:29:14 +0800

    上傳

---
+++ .vitepress/config.mjs
@@ -0,0 +1,73 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "SnowLin",
+  description: "測試網站",
+  themeConfig: {
+    outlineTitle: '本頁目錄',
+    outline: [2, 6],
+    lastUpdatedText: '更新日期',
+    logo: '/imgs/logo.png',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首頁', link: '/' },
+      { text: '範例', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: '演示 1',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: '演示 2',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜尋',
+            buttonAriaLabel: '搜尋',
+          },
+          modal: {
+            noResultsText: '無法找到相關項目',
+            resetButtonTitle: '清除查詢條件',
+            footer: {
+              selectText: '選擇',
+              navigateText: '切換',
+            },
+          },
+        },
+      },
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/snowlinouo/' },
+      { icon: 'youtube', link: 'https://www.youtube.com/c/nightsnowlin' },
+      { icon: 'facebook', link: 'https://www.facebook.com/SnowLinOuO' },
+      { icon: 'discord', link: 'https://discord.gg/W7P6UFQ' },
+      { icon: 'x', link: 'https://twitter.com/nightsnowlin' },
+      
+    ],
+
+    docFooter: {
+      prev: '上一頁',
+      next: '下一頁',
+    },
+
+    footer: {
+      copyright: "Copyright @ 2024 SnowLin",
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "SnowLin 網站"
+  text: "測試網站"
+  tagline: 一個測試用的網站
+  image:
+    src: /imgs/logo.png
+    alt: 雪鈴
+  actions:
+    - theme: brand
+      text: 前往網站
+      link: /markdown-examples
+    - theme: alt
+      text: 前往 GitHub
+      link: https://github.com/snowlinouo/Snowlin-VitePress
+
+features:
+  - title: 範例 A
+    details: 我是一串文字
+  - title: 範例 B
+    details: 我是一串文字
+  - title: 範例 C
+    details: 我是一串文字
+---
+
